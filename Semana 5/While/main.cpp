078c126997b50f510e109e6eac9411f270cd22b4
Alumno B303 <Alumno-B303@UCSP.AULAB>
2025-09-03 08:22:21 -0500

    Uso de while

---
+++ Semana 5/While/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+     /**int i = 1;
+    while (i*3 < 100){
+        cout << i*3 << endl;
+        i = i + 1;
+    }
+    **/
+    int i;
+    cout << "Ingrese numero"<< endl;
+    cin >> i;
+    a = 1;
+    d = 0;
+    b = 1;
+
+
+    while (a<=20){
+        while ( b <= 20){
+            if ((a%b) == 0){
+                d = d +1
+            }
+            if (d <= 2){
+                cout << a <<endl;
+
+            }
+            b = b +1
+        }
+        a = a+1
+    }
+
+
+
+
+    return 0;
+}
